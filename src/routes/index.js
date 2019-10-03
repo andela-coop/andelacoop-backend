2418c8c04e5d6e328a5c4ea9cb9d534c256665d9
Olarewaju Temtope Albert <tmtpalbert@gmail.com>
2019-10-03 16:54:21 +0100

    Revert "ch(setup): create backend repo"
    This reverts commit 6c7cbf65af46772fc67e1f41669823f46986b91f.

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import user from '../users/route'
-
-const app = express();
-
-app.use(user)
-
-export default app;
--- src/users/route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import UserController from '.'
-
-const router = express.Router();
-
-router.get('/users', UserController.getAllUser)
-
-export default router;
